4e11540d355fe684c9db8728b3384585f7e08677
Ege Uz <enkomaun@gmail.com>
2021-05-11 21:22:38 +0300

    sandcube v1

--- sandcube.js
+++ sandcube.js
@@ -1,180 +1,274 @@
 let sandcube;
 
 function setup() {
+  colorMode(HSB, 1)
   createCanvas(windowWidth, windowHeight)
-  // angleMode(DEGREES)
-  background(22)
-  sandcube = new Sandcube(createVector(width / 2, height / 2), 220, 40)
-  noLoop()
+  sandcube = new Sandcube(createVector(width / 2, height / 2), 220, 8)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+  sandcube.position = createVector(width / 2, height / 2)
 }
 
 function draw() {
-  //step 1: draw circle out of pixel chunks
-  //step 2: draw hexagon out of pixel chunks
+  clear()
+  background("#121212")
+  sandcube.update()
   sandcube.render()
-  // console.log(sandcube.grains.length)
+}
+
+function mousePressed() {
+  sandcube.isRotating = true;
+}
+
+function mouseReleased() {
+  sandcube.isRotating = false;
+  sandcube.mouseAngle = 0;
+  sandcube.prevMouseAngle = 0;
 }
 
 class Sandcube {
   constructor(position, radius, resolution) {
-    this.position = position;
+    this.position = position
     this.radius = radius
     this.resolution = resolution
-    this.hexagonLines = []
+    //color properties
+    this.color1 = this.generateColor()
+    this.color2 = this.generateColor()
+    this.color3 = this.generateColor()
+    this.maxSwaps = 1500
+    //hexagon properties
     this.hexagon = this.initHexagon()
+    this.hexMask = this.initHexMask()
+    //grid properties
+    this.rows = 2 * this.radius / this.resolution
+    this.cols = 2 * this.radius / this.resolution
     this.grid = this.initGrid()
-    // this.grains = this.initGrains()
+    //rotation properties
+    this.isRotating = false;
+    this.rotation = 0;
+    this.rotationVelocity = 0;
+    this.mouseAngle = 0;
+    this.prevMouseAngle = 0;
+    //next task: set colors to grid points
   }
 
+  //BASE METHODS 
+  render() {
+    push()
+    translate(this.position.x, this.position.y)
+    rotate(this.rotation)
+    this.renderGrid()
+    // this.renderHexagon()
+    this.renderHexMask()
+    pop()
+  }
+
+  update() {
+    this.updateRotation()
+    this.updateColors()
+  }
+
+  //INIT METHODS 
   initHexagon() {
     const hexagon = []
-
     for (let i = 0; i < 6; i++) {
       const angle = TWO_PI / 6 * i
       const x = sin(angle) * this.radius
       const y = cos(angle) * this.radius
       hexagon[i] = createVector(x, y)
     }
+    return hexagon
+  }
 
-    //also generate a bunch of hexagon lines
-    Object.values(hexagon).forEach((p1, i) => {
-      let p2 = i === 5 ? hexagon[0] : hexagon[i + 1]
-      this.hexagonLines.push({start: p1, end: p2})
-    })
+  initHexMask() {
+    const masks = []
+    //create masks out of simpler vertices so that the rectangular grid i
+    for (let i = 0; i < 6; i++) {
+      const point1 = this.hexagon[i].copy()
+      const point2 = i === 5 ? this.hexagon[0].copy() : this.hexagon[i + 1].copy()
+      const direction = i >= 3 ? -1 : 1 //direction
+      masks[i] = [
+        point1,
+        point2,
+        createVector((this.radius + 10) * direction, point2.y - (10 * direction)),
+        createVector((this.radius + 10) * direction, point1.y + (10 * direction))
+      ]
+    }
+    //add buffers
+    masks.push([
+      createVector(-this.radius, this.hexagon[0].y),
+      createVector(this.radius, this.hexagon[0].y),
+      createVector(-this.radius, this.hexagon[0].y + 10),
+      createVector(this.radius, this.hexagon[0].y + 10),
+    ])
 
-    return hexagon;
+    masks.push([
+      createVector(-this.radius, this.hexagon[3].y),
+      createVector(this.radius, this.hexagon[3].y),
+      createVector(this.radius, this.hexagon[3].y - 10),
+      createVector(this.radius, this.hexagon[3].y - 10),
+    ])
+
+    return masks;
   }
 
   initGrid() {
-    const grid = []
+    let grid = []
+    const size = this.resolution;
+    for (let x = -this.radius; x < this.radius; x += size) {
+      for (let y = -this.radius; y < this.radius; y += size) {
+        const points = [
+          createVector(x, y),
+          createVector(x + size, y),
+          createVector(x + size, y + size),
+          createVector(x, y + size)
+        ]
+        const pos = points[0]
+        const clr = this.getBlockBaseColor(points)
 
-    //generate grains
-    for (let x = -this.radius; x < this.radius; x += this.resolution) {
-      for (let y = -this.radius; y < this.radius; y += this.resolution) {
-        const pt = createVector(x, y)
-        grid.push(pt)
+        grid.push({ pos, clr, size })
+      }
+    }
+    return grid
+  }
+
+  //RENDER METHODS 
+  renderGrid() {
+    this.grid.forEach(({ pos, clr, size }) => {
+      if (clr) {
+        fill(clr)
+        stroke(clr)
+      }
+      rect(pos.x, pos.y, size)
+    })
+  }
+
+  renderHexagon() {
+    stroke("red")
+    strokeWeight(4)
+    noFill()
+    beginShape()
+    this.hexagon.forEach(point => {
+      vertex(point.x, point.y)
+    })
+    endShape(CLOSE)
+  }
+
+  renderHexMask() {
+    fill("#121212")
+    noStroke()
+    this.hexMask.forEach(mask => {
+      beginShape()
+      mask.forEach(point => {
+        vertex(point.x, point.y)
+      })
+      endShape(CLOSE)
+    })
+  }
+
+  //UPDATE METHODS 
+  updateRotation() {
+    if (this.isRotating) {
+      const dx = mouseX - width / 2
+      const dy = mouseY - height / 2
+      this.mouseAngle = atan2(dy, dx)
+      if (this.prevMouseAngle === 0) this.prevMouseAngle = this.mouseAngle
+      let deltaMouse = this.mouseAngle - this.prevMouseAngle
+      //limit rotation velocity
+      deltaMouse = deltaMouse > 2 ? 2 : deltaMouse < -2 ? -2 : deltaMouse;
+      this.rotationVelocity = deltaMouse
+      this.rotation += this.rotationVelocity
+      this.prevMouseAngle = this.mouseAngle
+    } else if (this.rotationVelocity !== 0) {
+      if (this.rotationVelocity < 0) {
+        this.rotationVelocity += this.rotationVelocity < -0.01 ? 0.01 : 0.00035
+        if (this.rotationVelocity >= 0) this.rotationVelocity = 0;
+      } else if (this.rotationVelocity > 0) {
+        this.rotationVelocity -= this.rotationVelocity > 0.01 ? 0.01 : 0.00035
+        if (this.rotationVelocity <= 0) this.rotationVelocity = 0;
+      }
+      this.rotation += this.rotationVelocity
+    }
+  }
+
+  updateColors() {
+    const speed = abs(this.rotationVelocity)
+    const numSwaps = map(speed, 0, 2, 0, this.maxSwaps)
+    for (let i = 0; i < numSwaps; i++) {
+      const block = random(this.grid)
+      const blockColor = block.clr;
+      const blockIndex = this.grid.indexOf(block)
+      const adjacentBlocks = this.getAdjacentBlocks(blockIndex)
+      const swapBlock = random(adjacentBlocks)
+      const swapColor = swapBlock.clr
+      block.clr = swapColor;
+      swapBlock.clr = blockColor;
+    }
+  }
+
+  //HELPER METHODS
+  getBlockBaseColor(points) {
+    const center = createVector(0, 0)
+    const segment1 = [center, this.hexagon[2], this.hexagon[1], this.hexagon[0]]
+    const segment2 = [center, this.hexagon[4], this.hexagon[3], this.hexagon[2]]
+    const segment3 = [center, this.hexagon[0], this.hexagon[5], this.hexagon[4]]
+
+    for (let i = 0; i < points.length; i++) {
+      const { x, y } = points[i]
+      if (this.contains(segment1, x, y)) {
+        return this.color1
+      } else if (this.contains(segment2, x, y)) {
+        return this.color2
+      } else if (this.contains(segment3, x, y)) {
+        return this.color3
       }
     }
 
-    grid.forEach((pt, index) => {
-      if (index !== 0) return;
-      //get filter point
-      const fp = createVector(pt.x + this.resolution / 2, pt.y + this.resolution / 2)
-
-      //get a 0-360 degree heading value
-      const heading = degrees(fp.heading()) >= 0 ?
-        degrees(fp.heading()) :
-        360 + degrees(fp.heading())
-
-      
-      //loop through hexagon lines
-      this.hexagonLines.forEach(hexline => {
-        const intersection = getIntersectionPoint(
-          fp.x, 
-          fp.y,
-          0,
-          0,
-          hexline.start.x,
-          hexline.start.y,
-          hexline.end.x,
-          hexline.end.y
-        )
-
-        console.log(intersection)
-      })
-
-
-
-      //get the corresponding hex points to compare with grid block
-      //get which angle quadrant it's in
-      // const p1 = floor(heading / 60)
-      // console.log(p1)
-      // console.log(heading / 60)
-      // const p2 = p1 === 5 ? 0 : p1 + 1;
-
-      // console.log(intersectingPoint)
-      
-
-
-      // const lerpCoefficient = map(heading, p1 * 60, p2 * 60, 0, 1)
-      // //get corresponding point on hexagon
-      // const cp = p5.Vector.lerp(this.hexagon[`p${p1}`], this.hexagon[`p${p2}`], lerpCoefficient)
-
-      // if (dist(fp.x, fp.y, 0, 0) > dist(cp.x, cp.y, 0, 0)) {
-      //   grid.splice(index, 1)
-      //   strokeWeight(1)
-      //   stroke("blue")
-      //   line(width / 2 + cp.x, height / 2 + cp.y, width / 2 + fp.x, height / 2 + fp.y)
-      //   stroke("green")
-      //   line(width / 2, height / 2, width / 2 + fp.x, height / 2 + fp.y)
-      //   fill(255)
-      // }
-    })
-
-
-
-
-
-    return grid;
+    const n = floor(random(1, 4))
+    return this[`color${n}`]
   }
 
+  contains(shape, x, y) {
+    //adapted from: https://rosettacode.org/wiki/Ray-casting_algorithm
+    let count = 0;
+    for (let i = 0; i < shape.length; i++) {
+      const v1 = shape[i];
+      const v2 = shape[(i + 1) % shape.length]
+      if (west(v1, v2, x, y)) ++count;
+    }
+    return count % 2;
 
-
-
-
-  // initGrains() {
-  //   const grains = []
-  //   for (let x = -this.radius / 2; x < this.radius / 2; x += this.resolution) {
-  //     for (let y = -this.radius / 2; y < this.radius / 2; y += this.resolution) {
-  //       const pt = createVector(x, y)
-  //       if (pt.mag() <= this.radius) {
-  //         grains.push(pt)
-  //       }
-  //     }
-  //   }
-  //   return grains;
-  // }
-
-  render() {
-    // push()
-    // fill("#00ff0033")
-    // // noStroke()
-    // translate(this.position.x, this.position.y)
-    // this.grid.forEach(({ x, y }) => {
-    //   fill(255)
-    //   rect(x, y, this.resolution)
-    // })
-    // pop()
-
-    push()
-    stroke("#ff000033")
-    strokeWeight(5)
-    beginShape()
-    translate(this.position.x, this.position.y)
-    Object.values(this.hexagon).forEach(({ x, y }) => {
-      noFill()
-      vertex(x, y)
-    })
-    endShape(CLOSE)
-    pop()
+    function west(v1, v2, x, y) {
+      if (v1.y <= v2.y) {
+        if (y <= v1.y || y > v2.y || x >= v1.x && x >= v2.x) {
+          return false;
+        } else if (x < v1.x && x < v2.x) {
+          return true;
+        } else {
+          return (y - v1.y) / (x - v1.x) > (v2.y - v1.y) / (v2.x - v1.x)
+        }
+      } else {
+        return west(v2, v1, x, y)
+      }
+    }
   }
 
-}
+  getAdjacentBlocks(i) {
+    const blocks = []
+    if (this.grid[i - 1]) blocks.push(this.grid[i - 1]) //w
+    if (this.grid[i + 1]) blocks.push(this.grid[i + 1]) // e
+    if (this.grid[i - this.cols]) blocks.push(this.grid[i - this.cols]) //n
+    if (this.grid[i - this.cols - 1]) blocks.push(this.grid[i - this.cols - 1]) //nw
+    if (this.grid[i - this.cols + 1]) blocks.push(this.grid[i - this.cols + 1]) //ne
+    if (this.grid[i + this.cols]) blocks.push(this.grid[i + this.cols]) //s
+    if (this.grid[i + this.cols - 1]) blocks.push(this.grid[i + this.cols - 1]) //sw
+    if (this.grid[i + this.cols + 1]) blocks.push(this.grid[i + this.cols + 1]) //se
+    return blocks
+  }
 
-
-function getIntersectionPoint(p0_x, p0_y, p1_x, p1_y, p2_x, p2_y, p3_x, p3_y) {
-  let s1_x = p1_x - p0_x;
-  let s1_y = p1_y - p0_y;
-  let s2_x = p3_x - p2_x;
-  let s2_y = p3_y - p2_y;
-
-  let s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
-  let t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
-
-  if (s >=0 && s <= 1 && t >= 0 && t <= 0) {
-    return createVector(p0_x + (t * s1_x), p0_y + t * s1_y)
-  } else {
-    return false;
+  generateColor() {
+    return color(random(), .6, .9)
   }
 }
-
